5a90599dca6acb055695777bcfe377197f070425
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2022-12-28 22:15:15 +0000

    feat : Exercice 1

---
+++ script.js
@@ -0,0 +1,21 @@
+// Exercice 1
+
+const currentYear = 2022;
+let personOneBirth = prompt("saisissez la première date de naissance :");
+let personTwoBirth = prompt("saisissez la seconde date de naissance :");
+
+let personOneAge = currentYear - personOneBirth;
+let personTwoAge = currentYear - personTwoBirth;
+let nbreYear;
+
+if (personOneAge > personTwoAge) {
+    nbreYear = personOneAge - 2*personTwoAge;
+}
+else {
+    nbreYear = personTwoAge - 2*personOneAge;
+}
+
+alert(`l'année est ${currentYear + nbreYear}`);
+
+// Exercice 2
+
